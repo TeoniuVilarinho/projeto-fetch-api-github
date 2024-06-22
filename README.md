b6496933f80229cdccd2f1a6ddb2806cabfd2a2e
André Vilarinho <andrevilarinho@gmail.com>
2024-06-22 20:24:47 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# Projeto de fetch usando api do GitHub
+
+- Projeto que consome a API do GitHub usando o fetch para buscar informações dos usuários e mostrar em tela.
+
+## ScreenShots
+
+- Screnshot do site
+
+    <img src="./src/design/projeto-fetch-api-github.png" alt="ScreenShot do site">
+
+## Tecnologias utilizadas
+
+<img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> <img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"> <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+
+## Link
+
+- Live Site: <a 
+                href="https://teoniuvilarinho.github.io/projeto-fetch-api-github" target="_blank">www.fetch-api-github.com
+            </a>
---
+++ src/js/objects/screen.js
@@ -0,0 +1,27 @@
+const screen = {
+    userProfile: document.querySelector('.profile-data'),
+    renderUser(user) {
+        this.userProfile.innerHTML = `<div class="info">
+                            <img src="${user.avatarUrl}" alt="Foto do perfil do usuário"/>
+                            <div class="data">
+                                <h1>Nome: ${user.name ?? 'Não possui nome cadastrado 😥'}</h1>
+                                <p>Bio: ${user.bio ?? 'Não possui bio cadastrada 😥'}</p>
+                            </div>`
+
+        let repositoriesItens = ''
+        user.repositories.forEach(repo => repositoriesItens += `<li> <a href="${repo.html_url}" target="_blank"> ${repo.name} </a> </li>`)
+
+        if(user.repositories.length > 0) {
+            this.userProfile.innerHTML += `<div class="repositories section">
+                                                <h2>Repositórios (10 mais recentes)</h2>
+                                                <ul>${repositoriesItens}</ul>
+                                            </div>`
+        }
+    },
+
+    renderNotFound() {
+        this.userProfile.innerHTML = "<h3>Usuário não encontrado ❌</h3>"
+    }
+}
+
+export { screen }
