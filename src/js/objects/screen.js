ba7fbf6cb504ea5669899865d9fc55f092329112
André Vilarinho <andrevilarinho@gmail.com>
2024-06-23 11:59:02 -0300

    Update screen.js

--- src/js/objects/screen.js
+++ src/js/objects/screen.js
@@ -1,12 +1,13 @@
 const screen = {
     userProfile: document.querySelector('.profile-data'),
     renderUser(user) {
-        this.userProfile.innerHTML = `<div class="info">
-                            <img src="${user.avatarUrl}" alt="Foto do perfil do usuário"/>
-                            <div class="data">
-                                <h1>Nome: ${user.name ?? 'Não possui nome cadastrado 😥'}</h1>
-                                <p>Bio: ${user.bio ?? 'Não possui bio cadastrada 😥'}</p>
-                            </div>`
+        this.userProfile.innerHTML =     `<div class="info">
+                                            <img src="${user.avatarUrl}" alt="Foto do perfil do usuário"/>
+                                            <div class="data">
+                                                <h1>Nome: ${user.name ?? 'Não possui nome cadastrado 😥'}</h1>
+                                                <p>Bio: ${user.bio ?? 'Não possui bio cadastrada 😥'}</p>
+                                            </div>
+                                          </div>`
 
         let repositoriesItens = ''
         user.repositories.forEach(repo => repositoriesItens += `<li> <a href="${repo.html_url}" target="_blank"> ${repo.name} </a> </li>`)
